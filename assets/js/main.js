a3e2f989ee0a10f4893c558213fd529a381ce467
FeliOdras <ariane.feldmann@odras.de>
2019-07-10 14:59:00 +0200

    added variants to object

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,16 @@ const app = new Vue({
     link: "https://me.odras.de",
     inventory: 100,
     onSale: true,
-    details: ["80% cotton", "20% polyester", "Gender-neutral"]
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantID: 2234,
+        variantColor: "green"
+      },
+      {
+        variantID: 2235,
+        variantColor: "blue"
+      }
+    ]
   }
 });
