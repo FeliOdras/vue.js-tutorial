b837292779e84aba86b38b97b026b670d71c6896
FeliOdras <ariane.feldmann@odras.de>
2019-07-10 14:15:02 +0200

    Added image to data

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ const app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    description: "These are nice and warm socks."
+    description: "These are nice and warm socks.",
+    image: "./assets/img/vmSocks-green-onWhite.jpg"
   }
 });
