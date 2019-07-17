5527ab6ed31b125d93ae0faa459c526c759a30ac
FeliOdras <ariane.feldmann@odras.de>
2019-07-17 09:28:42 +0200

    Added review tab

--- assets/js/main.js
+++ assets/js/main.js
@@ -209,6 +209,22 @@ Vue.component("product-review", {
     }
   }
 });
+
+Vue.component("product-tabs", {
+  template: `
+    <div>
+      <span class="tab" 
+            v-for="(tab, index) in tabs" :key="index">
+              {{ tab }}
+            </span>
+    </div>
+  `,
+  data() {
+    return {
+      tabs: ["Reviews", "Make a Review"]
+    };
+  }
+});
 const app = new Vue({
   el: "#app",
   data: {
